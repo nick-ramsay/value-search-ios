7947e8c8f9ebc8b8383dab67584e6383e87c832a
Nick Ramsay <NickRamsay1989@gmail.com>
2024-04-17 13:49:15 +1000

    Initial Commit

---
+++ value-search-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  value-search-ios
+//
+//  Created by Nick Ramsay on 17/4/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ value-search-iosUITests/value_search_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  value_search_iosUITestsLaunchTests.swift
+//  value-search-iosUITests
+//
+//  Created by Nick Ramsay on 17/4/2024.
+//
+
+import XCTest
+
+final class value_search_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
